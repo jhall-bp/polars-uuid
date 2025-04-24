7c497ae9cf2cf5dc971a1e8ec7bd58839774b090
Jesse Halley <jesse.halley@elastik.com>
2025-04-24 10:31:15 +1000

    feat: add additional test case

--- tests/test_uuid_v4.py
+++ tests/test_uuid_v4.py
@@ -4,13 +4,24 @@ from polars_uuid import uuid_v4
 
 UUID_PATTERN = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$"
 
-def test_uuid_v4():
-    df = (
-        pl.DataFrame({ "idx": list(range(10_000_000)) })
-        .with_columns(uuid=uuid_v4())
-    )
+
+def test_uuid_v4() -> None:
+    df = pl.DataFrame({"idx": list(range(10_000_000))}).with_columns(uuid=uuid_v4())
 
     assert df["uuid"].null_count() == 0
     assert df["uuid"].dtype == pl.String
     assert df["uuid"].is_unique().all()
     assert (df["uuid"].str.count_matches(UUID_PATTERN) == 1).all()
+
+
+def test_partial_uuid_v4() -> None:
+    df = pl.DataFrame({"idx": list(range(10_000_000))}).with_columns(
+        pl.when(pl.col("idx") % 2 == 0).then(uuid_v4()).otherwise("idx").alias("uuid")
+    )
+
+    assert df["uuid"].null_count() == 0
+    assert df["uuid"].dtype == pl.String
+    assert df["uuid"].is_unique().all()
+    assert df["uuid"].str.count_matches(UUID_PATTERN).is_in((0, 1)).all()
+    assert (df["uuid"].str.count_matches(UUID_PATTERN)).sum() == (df.height / 2)
+    assert df["uuid"].cast(pl.Int32, strict=False).null_count() == (df.height / 2)
