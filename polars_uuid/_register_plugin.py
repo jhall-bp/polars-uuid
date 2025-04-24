e3916884436c1283e98b6beaa18978d5e125c98a
Jesse Halley <jesse.halley@elastik.com>
2025-04-24 12:46:08 +1000

    feat: put package version in pyproject.toml file

--- polars_uuid/_register_plugin.py
+++ polars_uuid/_register_plugin.py
@@ -5,9 +5,7 @@ from pathlib import Path
 import polars as pl
 from polars.plugins import register_plugin_function
 
-from polars_uuid._internal import __version__ as __version__
-
-LIB = Path(__file__).parent
+_LIB = Path(__file__).parent
 _ARGS = (
     pl.repeat(
         pl.lit("", dtype=pl.String),
@@ -18,7 +16,7 @@ _ARGS = (
 def uuid_v4() -> pl.Expr:
     return register_plugin_function(
         args=_ARGS,
-        plugin_path=LIB,
+        plugin_path=_LIB,
         function_name="uuid4_rand",
         is_elementwise=True,
     )
