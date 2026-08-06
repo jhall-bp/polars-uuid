d43fed7e71bea5c1ccfcf1a5b23ebd5a7999154c
Jesse Halley <jesse.halley@elastik.com>
2026-08-06 10:16:57 +1000

    feat: provide explicit path to rust plugin

--- polars_uuid/_constants.py
+++ polars_uuid/_constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import polars as pl
 
 
-PLUGIN_PATH = Path(__file__).parent
+PLUGIN_PATH = Path(__file__).parent.joinpath("_internal.abi3.so").resolve(strict=True)
 
 ARGS = pl.first()
 ARGS_SINGLE = pl.lit(None, dtype=pl.Null)
